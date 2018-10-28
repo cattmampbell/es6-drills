6ccc8ddcac618ef869805ddb8c5187cbcf00bb33
Matthew Campbell <cattmampbell@gmail.com>
2018-10-28 18:02:44 -0400

    Connected index.html and script.js files.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Run on load...
+$(document).ready(() => {
+    // console.log(`Testing... Testing... 1, 2, 3, ES6 Drills!`);
+    
+    // Should log: Start of script.js file!
+    console.log(`Start of script.js file!`);
+
+
+
+    // Should log: End of scripts.js file!
+    console.log(`End of scripts.js file!`);
+});
