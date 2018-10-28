f7ef05238b7654dfa474828e3b3f775a00ff4c58
Matthew Campbell <cattmampbell@gmail.com>
2018-10-28 18:14:23 -0400

    Created favoriteMovie function in scripts.js file.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,22 @@ $(document).ready(() => {
     // Should log: Start of script.js file!
     console.log(`Start of script.js file!`);
 
+    /* Template Literals and Default Parameters Value: */
 
+    let favoriteMovie = (name = `World`, movie = `The Room`) => { console.log(`My name is ${name} and my favorite movie is ${movie}.`); };
+
+    // Should log: My name is World and my favorite movie is The Room.
+    favoriteMovie();
+
+    // Should log: My name is World and my favorite movie is Die Hard.
+    favoriteMovie(`Die Hard`);
+
+    // Should log: My name is Matthew and my favorite movie is Die Hard.
+    favoriteMovie(`Matthew`, `Die Hard`);
+
+    /* Arrow Functions: */
+    // Should log: My name is Olivia and my favorite movie is A Bug's Life.
+    favoriteMovie(`Olivia`, `A Bug's Life`);
 
     // Should log: End of scripts.js file!
     console.log(`End of scripts.js file!`);
